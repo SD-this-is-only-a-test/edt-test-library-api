90d11cd3e14ebe35a92924264d5efd0bcf507611
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-04 11:27:43 +0100

    Added class for books with basic properties ID and Title

---
+++ EdtTest.Data/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdtTest.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+    }
+}
