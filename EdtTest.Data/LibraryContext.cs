27956b216bc5f51ccc48bf38d3220310bef12add
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-04 11:39:34 +0100

    Added books as a data set to the context

--- EdtTest.Data/LibraryContext.cs
+++ EdtTest.Data/LibraryContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EdtTest.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EdtTest.Data
 {
     public class LibraryContext : DbContext
     {
-
+        public DbSet<Book> Books { get; set; }
     }
 }
