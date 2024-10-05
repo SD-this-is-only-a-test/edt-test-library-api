f8fc52f492ada23e374142d8708ce497e82b2016
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-05 17:49:05 +0100

    Added book copy to models.  Library may have multiple copies of the same book.

---
+++ EdtTest.Data/Models/BookCopy.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdtTest.Data.Models
+{
+    public class BookCopy
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int BookID { get; set; }
+
+        public virtual Book Book { get; set; }
+    }
+}
