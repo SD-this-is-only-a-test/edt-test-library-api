e10785188f5e37e8504cc27db75f131b25ea857c
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-05 18:12:46 +0100

    Added book copies and loans to the context

--- EdtTest.Data/LibraryContext.cs
+++ EdtTest.Data/LibraryContext.cs
@@ -6,6 +6,8 @@ namespace EdtTest.Data
     public class LibraryContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+        public DbSet<BookCopy> BookCopies { get; set; }
+        public DbSet<BookLoan> BookLoans { get; set; }
         public DbSet<LibraryMember> Members { get; set; }
     }
 }
