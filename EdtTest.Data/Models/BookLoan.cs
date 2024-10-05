5c5dd465335cc2e1960f7a4c6318730b660102c9
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-05 18:07:49 +0100

    Added book loan to the models.  This represents the loan of a copy of a book to a library member

---
+++ EdtTest.Data/Models/BookLoan.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EdtTest.Data.Models
+{
+    public class BookLoan
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey(nameof(Copy))]
+        public int CopyID { get; set; }
+
+        [ForeignKey(nameof(Member))]
+        public int MemberID { get; set; }
+
+        /// <summary>
+        /// The date that the book should be returned by
+        /// </summary>
+        public DateTime ReturnByDate { get; set; }
+
+        /// <summary>
+        /// The date that the book copy was returned, otherwise null
+        /// </summary>
+        public DateTime? ReturnedDate { get; set; }
+
+        /// <summary>
+        /// The book copy which has been loaned out
+        /// </summary>
+        public virtual BookCopy Copy { get; set; }
+
+        /// <summary>
+        /// The library member who has borrowed the book
+        /// </summary>
+        public virtual LibraryMember Member { get; set; }
+    }
+}
