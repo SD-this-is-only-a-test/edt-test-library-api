634859522264cb2ffc5c5e37e47629e930ab9f60
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-05 17:39:27 +0100

    Added library member class.  People who borrow books

---
+++ EdtTest.Data/Models/LibraryMember.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdtTest.Data.Models
+{
+    public class LibraryMember
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Forename { get; set; }
+
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// We may require the DoB if the library were to have age restricted content
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+    }
+}
