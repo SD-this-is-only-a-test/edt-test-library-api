69583dd9ebc5ca47a56b942e0119daca5b23e80d
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 11:13:43 +0100

    Added IBooksService as a constructor parameter for BooksController

--- EdtTest.LibraryAPI/Controllers/BooksController.cs
+++ EdtTest.LibraryAPI/Controllers/BooksController.cs
@@ -1,11 +1,12 @@
 ﻿using EdtTest.Data.Models;
+using EdtTest.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EdtTest.LibraryAPI.Controllers
 {
     [ApiController]
     [Route("[Controller]")]
-    public class BooksController(ILoggerFactory loggerFactory) : ControllerBase
+    public class BooksController(ILoggerFactory loggerFactory, IBooksService booksService) : ControllerBase
     {
         private readonly ILogger _logger = loggerFactory.CreateLogger<BooksController>();
 
