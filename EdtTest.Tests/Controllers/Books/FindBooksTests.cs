caff469124fe74fdefc6d9020d38ab126c960569
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 14:36:41 +0100

    Added FindBooksTests class

---
+++ EdtTest.Tests/Controllers/Books/FindBooksTests.cs
@@ -0,0 +1,24 @@
+﻿using EdtTest.Data.Filters;
+using EdtTest.LibraryAPI.Controllers;
+using EdtTest.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace EdtTest.Tests.Controllers.Books
+{
+    public class FindBooksTests
+    {
+        [Test]
+        public void Result_IsNot_Null()
+        {
+            Mock<ILoggerFactory> mLoggerFactory = new Mock<ILoggerFactory>();
+            Mock<IBooksService> mBooksService = new Mock<IBooksService>();
+
+            var controller = new BooksController(mLoggerFactory.Object, mBooksService.Object);
+
+            var result = controller.FindBooks(new BookFilter());
+
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
