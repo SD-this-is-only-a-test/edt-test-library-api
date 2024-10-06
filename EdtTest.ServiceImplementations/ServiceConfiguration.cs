a8cf92c49222ae6b6d007fb1cfd35eef0d6f1d3b
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 13:10:15 +0100

    Added a service configuration class to be used for implementation configuration

---
+++ EdtTest.ServiceImplementations/ServiceConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace EdtTest.ServiceImplementations
+{
+    /// <summary>
+    /// This will contain an extension method or methods for setting up all of our service implementations.
+    /// </summary>
+    public static class ServiceConfiguration
+    {
+    }
+}
