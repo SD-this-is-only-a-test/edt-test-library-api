4de6b20590f5e560cae74c7f85f4a2fd3891a43e
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 16:05:27 +0100

    Changed type for local parameter of books so it could be used to mock DbSet for tests.  AddedMock DbSet and DbContext. Instatniated service, captured result, and tested for exclusion of book on loan.

--- EdtTest.Tests/Services/Books/FindBooksTests.cs
+++ EdtTest.Tests/Services/Books/FindBooksTests.cs
@@ -62,13 +62,32 @@ namespace EdtTest.Tests.Services.Books
                 ]
             };
 
-            Book[] books = [
+            var books = new Book[]
+            {
                 bookOnLoan,
+                // this book has never been on loan
                 new Book { ID = 2, Title = "BookB", Authors = "AuthorA", Copies = [ new BookCopy { ID = 2, BookID = 2, Loans = [] } ] },
+                // this book has been on loan but has been returned
                 new Book { ID = 3, Title = "BookC", Authors = "AuthorC", Copies = [ new BookCopy {  ID = 3, BookID = 3, Loans = [ new BookLoan { ID = 2, CopyID = 3, MemberID = 1, ReturnByDate = DateTime.Now.AddDays(-7), ReturnedDate = DateTime.Now.AddDays(-8)}] }]}
-            ];
+            }.AsQueryable();
 
-            Assert.Fail("Still building test");
+
+            var filter = new BookFilter { AvailableForLoanOnly = true };
+            Mock<DbSet<Book>> mBooks = new Mock<DbSet<Book>>();
+            Mock<LibraryContext> mContext = new Mock<LibraryContext>();
+
+            mBooks.As<IQueryable<Book>>().Setup(b => b.Provider).Returns(books.Provider);
+            mBooks.As<IQueryable<Book>>().Setup(b => b.Expression).Returns(books.Expression);
+            mBooks.As<IQueryable<Book>>().Setup(b => b.ElementType).Returns(books.ElementType);
+            mBooks.As<IQueryable<Book>>().Setup(b => b.GetEnumerator()).Returns(books.GetEnumerator());
+
+            mContext.Setup(c => c.Books).Returns(mBooks.Object);
+
+            BooksService service = new BooksService(mContext.Object);
+
+            var serviceResult = service.FindBooks(filter);
+
+            CollectionAssert.DoesNotContain(serviceResult, bookOnLoan);
         }
     }
 }
