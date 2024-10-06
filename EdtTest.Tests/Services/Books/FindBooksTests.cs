7232b4117072477ee9898946b865b4211df33e48
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 15:12:43 +0100

    Added a test class to test BooksService.FindBooks.  Unable to test because of missing parameterless constructor

---
+++ EdtTest.Tests/Services/Books/FindBooksTests.cs
@@ -0,0 +1,22 @@
+﻿using EdtTest.Data;
+using EdtTest.Data.Filters;
+using EdtTest.ServiceImplementations.Services;
+using Moq;
+
+namespace EdtTest.Tests.Services.Books
+{
+    public class FindBooksTests
+    {
+        [Test]
+        public void ServiceResult_IsNot_Null()
+        {
+            Mock<LibraryContext> mContext = new Mock<LibraryContext>();
+            BooksService service = new BooksService(mContext.Object);
+            var filter = new BookFilter();
+
+            var result = service.FindBooks(filter);
+
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
