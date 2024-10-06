e1ae6e2d92958ba0a5beab8aa827ed2cb95cccb0
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 11:00:37 +0100

    Added a books controller for actions associated with books

---
+++ EdtTest.LibraryAPI/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using EdtTest.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EdtTest.LibraryAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class BooksController : ControllerBase
+    {
+        public IEnumerable<Book> Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
