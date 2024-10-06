ffbffbae9fcb7d12aafb4912cc3fcf66650bf04c
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 11:12:48 +0100

    Added ILoggerFactory as a constructor parameter and  ILogger as a field for BooksController so we can log application errors.

--- EdtTest.LibraryAPI/Controllers/BooksController.cs
+++ EdtTest.LibraryAPI/Controllers/BooksController.cs
@@ -5,8 +5,10 @@ namespace EdtTest.LibraryAPI.Controllers
 {
     [ApiController]
     [Route("[Controller]")]
-    public class BooksController : ControllerBase
+    public class BooksController(ILoggerFactory loggerFactory) : ControllerBase
     {
+        private readonly ILogger _logger = loggerFactory.CreateLogger<BooksController>();
+
         public IEnumerable<Book> Index()
         {
             throw new NotImplementedException();
