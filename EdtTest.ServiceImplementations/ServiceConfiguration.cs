da31626cf30fbf6d596cbb570ec39ebf85a7ca43
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 13:15:08 +0100

    Added a static method for setting up application services

--- EdtTest.ServiceImplementations/ServiceConfiguration.cs
+++ EdtTest.ServiceImplementations/ServiceConfiguration.cs
@@ -1,9 +1,16 @@
-﻿namespace EdtTest.ServiceImplementations
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EdtTest.ServiceImplementations
 {
     /// <summary>
     /// This will contain an extension method or methods for setting up all of our service implementations.
     /// </summary>
     public static class ServiceConfiguration
     {
+        public static void ConfigureServiceImplementations(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
     }
 }
