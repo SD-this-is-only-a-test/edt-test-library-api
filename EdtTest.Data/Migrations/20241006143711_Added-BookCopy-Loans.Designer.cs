7a50a117d9d672cfe36adb1271018eab8d2dd502
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 15:38:46 +0100

    Added a new migration to include the relationship between copies and loans

--- EdtTest.Data/Migrations/20241006143711_Added-BookCopy-Loans.Designer.cs
+++ EdtTest.Data/Migrations/20241006143711_Added-BookCopy-Loans.Designer.cs
@@ -4,6 +4,7 @@ using EdtTest.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EdtTest.Data.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20241006143711_Added-BookCopy-Loans")]
+    partial class AddedBookCopyLoans
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
