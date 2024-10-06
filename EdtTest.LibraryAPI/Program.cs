3e966f908c470b0c442debfeae547febf0668382
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 17:44:48 +0100

    Added JSON options to ignore cyclic references

--- EdtTest.LibraryAPI/Program.cs
+++ EdtTest.LibraryAPI/Program.cs
@@ -10,7 +10,9 @@ namespace EdtTest.LibraryAPI
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddJsonOptions(o => o.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
