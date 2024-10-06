2d3b10002a18f5545031c3ad577164e2055f78b0
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 17:08:45 +0100

    Added a class to represent a request to create a new book

---
+++ EdtTest.LibraryAPI/Models/CreateBookRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdtTest.LibraryAPI.Models
+{
+    public class CreateBookRequest
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Authors { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public bool AddCopy { get; set; }
+    }
+}
