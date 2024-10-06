f23ee6519957b6ea74c245d91849b55744513b7a
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 11:41:51 +0100

    Added test for book service throwing error for get books.  It passed because we initialise errors to an empty array.  We need to make this nullable or test the length.

--- EdtTest.Tests/Controllers/Books/IndexTests.cs
+++ EdtTest.Tests/Controllers/Books/IndexTests.cs
@@ -25,5 +25,21 @@ namespace EdtTest.Tests.Controllers.Books
 
             Assert.That(result, Is.Not.Null);
         }
+
+        [Test]
+        public void ResultErrors_IsNot_Null_When_BookService_Throws_Error()
+        {
+            Mock<ILoggerFactory> mLoggerFactory = new Mock<ILoggerFactory>();
+            Mock<IBooksService> mBooksService = new Mock<IBooksService>();
+            string errorMessage = $"Book service error {DateTime.Now.Ticks}";
+
+            mBooksService.Setup(m => m.GetBooks()).Throws(new Exception(errorMessage));
+
+            var controller = new BooksController(mLoggerFactory.Object, mBooksService.Object);
+
+            var result = controller.Index();
+
+            Assert.That(result.Errors, Is.Not.Null);
+        }
     }
 }
