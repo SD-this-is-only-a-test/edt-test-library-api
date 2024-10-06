38909dacb6f1f42bd68922682c896ccca90cf12c
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 11:23:28 +0100

    Added books controller index tests class

---
+++ EdtTest.Tests/Controllers/Books/IndexTests.cs
@@ -0,0 +1,17 @@
+﻿namespace EdtTest.Tests.Controllers.Books
+{
+    public class IndexTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Result_IsNot_Null()
+        {
+            Assert.Fail("We have not tested yet!");
+        }
+    }
+}
