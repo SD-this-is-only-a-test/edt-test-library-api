75b257c7f061b8f55ed07121157761a9a975a339
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 14:45:34 +0100

    Added default implementation of IBooksService.FindBooks to BooksService

--- EdtTest.ServiceImplementations/Services/BooksService.cs
+++ EdtTest.ServiceImplementations/Services/BooksService.cs
@@ -1,12 +1,18 @@
 ﻿using EdtTest.Data.Models;
 using EdtTest.Data;
 using EdtTest.Services;
+using EdtTest.Data.Filters;
 
 namespace EdtTest.ServiceImplementations.Services
 {
     public class BooksService(LibraryContext context) : IBooksService
     {
         private readonly LibraryContext _context = context;
+
+        public IEnumerable<Book> FindBooks(BookFilter filter)
+        {
+            throw new NotImplementedException();
+        }
 
         public IEnumerable<Book> GetBooks()
         {
