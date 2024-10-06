beef6120ccf368a0f40e22c8e31359057acce8c5
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 13:03:40 +0100

    Added default implementation to BooksService of IBooksService.GetBooks

--- EdtTest.ServiceImplementations/BooksService.cs
+++ EdtTest.ServiceImplementations/BooksService.cs
@@ -1,9 +1,13 @@
-﻿using EdtTest.Services;
+﻿using EdtTest.Data.Models;
+using EdtTest.Services;
 
 namespace EdtTest.ServiceImplementations
 {
     public class BooksService : IBooksService
     {
-
+        public IEnumerable<Book> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
