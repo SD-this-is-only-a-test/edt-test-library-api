e7d8df9620a555e893b0b58dc661ab2020981bef
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 14:44:40 +0100

    Added a definition for FindBooks to the BooksService interface

--- EdtTest.Services/IBooksService.cs
+++ EdtTest.Services/IBooksService.cs
@@ -1,9 +1,11 @@
-﻿using EdtTest.Data.Models;
+﻿using EdtTest.Data.Filters;
+using EdtTest.Data.Models;
 
 namespace EdtTest.Services
 {
     public interface IBooksService
     {
         IEnumerable<Book> GetBooks();
+        IEnumerable<Book> FindBooks(BookFilter filter);
     }
 }
