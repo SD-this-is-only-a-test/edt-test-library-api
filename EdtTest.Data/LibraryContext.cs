de3e5765ba1adac58dab795af9c4dfe3fe022e84
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 01:12:07 +0100

    Aded constructor with context options

--- EdtTest.Data/LibraryContext.cs
+++ EdtTest.Data/LibraryContext.cs
@@ -5,6 +5,11 @@ namespace EdtTest.Data
 {
     public class LibraryContext : DbContext
     {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options)
+        {
+        }
+
         public DbSet<Book> Books { get; set; }
         public DbSet<BookCopy> BookCopies { get; set; }
         public DbSet<BookLoan> BookLoans { get; set; }
