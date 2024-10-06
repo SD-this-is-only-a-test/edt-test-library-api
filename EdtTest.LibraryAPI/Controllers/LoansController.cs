12e591fd480a9fddaa47de250018ebc2981c87bf
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-06 18:17:32 +0100

    Added a controller for managing book loans

---
+++ EdtTest.LibraryAPI/Controllers/LoansController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EdtTest.LibraryAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]/[action]")]
+    public class LoansController : ControllerBase
+    {
+
+    }
+}
