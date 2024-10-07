9178599b69c8235cb2ff6a96e8785630878a3bdf
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-07 09:37:39 +0100

    Added classes for requests to start and end loans

---
+++ EdtTest.LibraryAPI/Models/EndLoanRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdtTest.LibraryAPI.Models
+{
+    public class EndLoanRequest
+    {
+        [Required]
+        public int BookCopyID { get; set; }
+    }
+}
---
+++ EdtTest.LibraryAPI/Models/StartLoanRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdtTest.LibraryAPI.Models
+{
+    public class StartLoanRequest
+    {
+        [Required]
+        public int BookCopyID { get; set; }
+
+        [Required]
+        public int LibraryMemberID { get; set; }
+    }
+}
