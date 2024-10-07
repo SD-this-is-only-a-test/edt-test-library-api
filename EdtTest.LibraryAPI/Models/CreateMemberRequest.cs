6324d4b1de421587bc803798a033f5e250fa351f
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-07 07:24:53 +0100

    Added a class to contain data for creating a member

---
+++ EdtTest.LibraryAPI/Models/CreateMemberRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdtTest.LibraryAPI.Models
+{
+    public class CreateMemberRequest
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Title { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Forename { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Surname { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
