25a8f7ce0d1a896f02ecb1f608cc0de0ae775aac
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-07 07:24:03 +0100

    Added service definition for members service

---
+++ EdtTest.Services/IMembersService.cs
@@ -0,0 +1,12 @@
+﻿using EdtTest.Data.Filters;
+using EdtTest.Data.Models;
+
+namespace EdtTest.Services
+{
+    public interface IMembersService
+    {
+        IEnumerable<LibraryMember> GetMembers();
+        IEnumerable<LibraryMember> FindMembers(MemberFilter filter);
+        LibraryMember CreateMember(string title, string forename, string surname, DateTime dateOfBirth);
+    }
+}
