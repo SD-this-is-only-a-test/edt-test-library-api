3fae5ace5f35847a0103e185c4b99bdac0079c54
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-07 09:18:55 +0100

    Added ILoggerFactory and IBookLoansService as constructor parameters for the LoansController.  Added ILogger and IBookLoansService as private readonly fields of LoansController.

--- EdtTest.LibraryAPI/Controllers/LoansController.cs
+++ EdtTest.LibraryAPI/Controllers/LoansController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EdtTest.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EdtTest.LibraryAPI.Controllers
 {
     [ApiController]
     [Route("[Controller]/[action]")]
-    public class LoansController : ControllerBase
+    public class LoansController(ILoggerFactory loggerFactory, IBookLoansService bookLoansService) : ControllerBase
     {
+        private readonly ILogger<LoansController> _logger = loggerFactory.CreateLogger<LoansController>();
 
+        private readonly IBookLoansService _bookLoansService = bookLoansService;
     }
 }
