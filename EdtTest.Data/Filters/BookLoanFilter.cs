27a5f3776be03534399e9883f7cb277eec8592f8
Stuart Dandy <20288190+criminal-jelly@users.noreply.github.com>
2024-10-07 08:56:15 +0100

    Added book loand filter and book loan state enum for searching for book loans

---
+++ EdtTest.Data/Filters/BookLoanFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace EdtTest.Data.Filters
+{
+    public class BookLoanFilter
+    {
+        public int? BookCopyID { get; set; }
+        public int? LibraryMemberID { get; set; }
+        public BookLoanState LoanState { get; } = BookLoanState.All;
+    }
+}
---
+++ EdtTest.Data/Filters/BookLoanState.cs
@@ -0,0 +1,18 @@
+﻿namespace EdtTest.Data.Filters
+{
+    public enum BookLoanState : byte
+    {
+        /// <summary>
+        /// All loans regardless of return state.
+        /// </summary>
+        All = 0,
+        /// <summary>
+        /// Loans where the copy has not been returned.
+        /// </summary>
+        NotReturned = 1,
+        /// <summary>
+        /// Loans where the copy has been returned.
+        /// </summary>
+        Returned = 2,
+    }
+}
